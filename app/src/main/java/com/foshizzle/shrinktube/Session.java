d311bcfdabd853a17831d0cbdb6e8887c8e01c5e
Arnab <asaha@cuttingchaitech.com>
2018-01-31 15:32:24 +0530

    - ShrinkTube First Commit

---
+++ app/src/main/java/com/foshizzle/shrinktube/Session.java
@@ -0,0 +1,25 @@
+package com.foshizzle.shrinktube;
+
+public class Session{
+
+//    private static YouTubePlayerView youTubePlayerView;
+
+//
+//    public Session() {
+//    }
+//
+//    public Session(YouTubePlayerView youTubePlayerView) {
+//
+//        this.youTubePlayerView = youTubePlayerView;
+//
+//    }
+//
+//    public static YouTubePlayerView getYouTubePlayerView() {
+//        return youTubePlayerView;
+//    }
+//
+//    public static void setYouTubePlayerView(YouTubePlayerView View) {
+//        youTubePlayerView = View;
+//    }
+
+}
